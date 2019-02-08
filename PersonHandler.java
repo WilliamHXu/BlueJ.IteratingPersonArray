99bf2c194d4ff3a7b88a0e49fa9010b560a521d5
WilliamXu <williamxu@zipcoders-MacBook-Pro-90.local>
2019-02-08 14:11:40 -0500

    Finished Lab

--- PersonHandler.java
+++ PersonHandler.java
@@ -1,4 +1,4 @@
-package com.zipcodewilmington;
+ 
 
 /**
  * Created by leon on 1/24/18.
@@ -13,14 +13,17 @@ public class PersonHandler {
     public String whileLoop() {
         String result = "";
         // assume there is a `counter`
+        int counter = 0;
         // while `counter` is less than length of array
+        while (counter < personArray.length) {
             // begin loop
-
+            result = result + personArray[counter].toString();
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
-
+                counter++;
             // end loop
+        }
         return result;
     }
 
@@ -31,14 +34,15 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
-
+        for (int i = 0; i < personArray.length; i++){
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
-
+            result = result + personArray[i].toString();
+        }
         return result;
     }
 
@@ -48,13 +52,14 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
-
+        for (Person i : personArray) {
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
-
+            result = result + i.toString();
+        }
         return result;
     }
 
